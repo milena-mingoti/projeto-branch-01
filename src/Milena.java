2b1fbc284f398ed1acb05adf9ae6f5a2b8c69332
Milena Mingoti <milenamingoti1@gmail.com>
2023-05-25 21:50:52 -0300

    terceiro commit

--- src/Milena.java
+++ src/Milena.java
@@ -1,6 +1,7 @@
 public class Milena {
     public static void main(String[] args) {
         System.out.println("Sou eu de novo! Fiquem com Deus");
+        System.out.println("Apenas que busquem conhecimento. Bilu");
 
     }
 }
